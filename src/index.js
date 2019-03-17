aab101ac1c5b38566bd506e2fe688ac8595903e9
Denasius <den_kostyuk1989@mail.ru>
2019-03-17 07:24:57 +0300

    created functionality for navigation menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-async function app (){
-  await fetch('babel')
-}
+import {HeaderComponent} from './components/header.component'
+import {NavigationComponent} from './components/navigation.component'
+import {CreateComponent} from './components/create.component'
+import {FavoriteComponent} from './components/favorite.component'
+import {PostsComponent} from './components/posts.component'
+
+new HeaderComponent('header')
+const navigation = new NavigationComponent('navigation')
+
+const posts = new PostsComponent('posts')
+const create = new CreateComponent('create')
+const favorite = new FavoriteComponent('favorite')
